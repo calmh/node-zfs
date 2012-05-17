3fd54858a062f71f658811b5bd0871e52a06f6c8
Jakob Borg <jakob@nym.se>
2012-05-17 18:58:01 +0000

    Do not use uncommon zfs list -p (fixes #1).
    zfs list -p is super nice but not implemented in the Oracle builds.

--- test/zfs.js
+++ test/zfs.js
@@ -9,24 +9,34 @@ describe('zfs', function () {
         it('returns a list of ZFS filesystems', function (done) {
             var expected = [ {
                 name: 'zones',
-                used: 1004799731712,
-                avail: 956926580736,
+                used: 1016833507328,
+                avail: 944892805120,
                 refer: 328704,
                 mountpoint: '/zones'
-            },
-            {
-                name: 'zones/f78f9208-9c26-47f7-9e03-881a96d17c04',
-                used: 590843904,
-                avail: 10146574336,
-                refer: 694054400,
-                mountpoint: '/zones/f78f9208-9c26-47f7-9e03-881a96d17c04'
-            },
-            {
-                name: 'zones/f78f9208-9c26-47f7-9e03-881a96d17c04/data',
-                used: 73728,
-                avail: 10146574336,
+            }, {
+                name: 'zones/37a67136-6cdf-4f65-add2-85e4fce60d63',
+                used: 696254464,
+                avail: 10039486054,
+                refer: 696254464,
+                mountpoint: '/zones/37a67136-6cdf-4f65-add2-85e4fce60d63'
+            }, {
+                name: 'zones/37a67136-6cdf-4f65-add2-85e4fce60d63-disk0',
+                used: 1503238553,
+                avail: 944892805120,
+                refer: 1116691496,
+                mountpoint: '-'
+            }, {
+                name: 'zones/37a67136-6cdf-4f65-add2-85e4fce60d63/cores',
+                used: 31744,
+                avail: 2415919104,
                 refer: 31744,
-                mountpoint: '/data'
+                mountpoint: '/zones/37a67136-6cdf-4f65-add2-85e4fce60d63/cores'
+            }, {
+                name: 'zones/39a4f744-85cf-416c-ad37-1b933f9e7b13',
+                used: 155648,
+                avail: 10737418240,
+                refer: 124928,
+                mountpoint: '/zones/39a4f744-85cf-416c-ad37-1b933f9e7b13'
             } ];
             zfs.list(function (err, list) {
                 should.not.exist(err);
@@ -36,21 +46,19 @@ describe('zfs', function () {
         });
         it('returns a list of ZFS snapshots', function (done) {
             var expected = [ {
-                name: 'zones/f78f9208-9c26-47f7-9e03-881a96d17c04/data@daily-20120430',
-                used: 1024,
-                refer: 32768,
+                name: 'zones/var@daily-20120516T043000Z',
+                used: 137216,
+                refer: 2820669,
                 mountpoint: '-'
-            },
-            {
-                name: 'zones/f78f9208-9c26-47f7-9e03-881a96d17c04/data@daily-20120501',
-                used: 1024,
-                refer: 32768,
+            }, {
+                name: 'zones/var@hourly-20120516T160000Z',
+                used: 39424,
+                refer: 2831155,
                 mountpoint: '-'
-            },
-            {
-                name: 'zones/f78f9208-9c26-47f7-9e03-881a96d17c04/data@daily-20120502',
-                used: 1024,
-                refer: 32768,
+            }, {
+                name: 'zones/var@hourly-20120516T170000Z',
+                used: 39424,
+                refer: 2831155,
                 mountpoint: '-'
             } ];
             zfs.list({ type: 'snapshot' }, function (err, list) {
@@ -67,20 +75,17 @@ describe('zfs', function () {
                 property: 'compression',
                 value: 'on',
                 source: 'local'
-            },
-            {
+            }, {
                 name: 'zones/f78f9208-9c26-47f7-9e03-881a96d17c04',
                 property: 'compression',
                 value: 'on',
                 source: 'inherited'
-            },
-            {
+            }, {
                 name: 'zones/f78f9208-9c26-47f7-9e03-881a96d17c04/data',
                 property: 'compression',
                 value: 'on',
                 source: 'inherited'
-            },
-            {
+            }, {
                 name: 'zones/f78f9208-9c26-47f7-9e03-881a96d17c04/data@daily-20120430',
                 property: 'compression',
                 value: '-',
